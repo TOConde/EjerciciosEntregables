5de587abc6db6d395b0e7526b992aef1aa7b6f50
TOConde <tomy_t.c@hotmail.com>
2023-09-14 13:04:05 -0300

    modifico agregarCafe y aplico los metodos

--- cafetera.js
+++ cafetera.js
@@ -32,16 +32,37 @@ var Cafetera = /** @class */ (function () {
         console.log("Se vaci\u00F3 la cafetera!");
     };
     Cafetera.prototype.agregarCafe = function (cantidad) {
-        this._cantidadActual = cantidad;
-        console.log("Se agrego ".concat(cantidad, "cc de cafe."));
+        if (cantidad <= this._capacidadMaxima) {
+            this._cantidadActual = cantidad;
+            console.log("Se agrego ".concat(cantidad, "cc de cafe."));
+        }
+        else {
+            console.log("Trataste sobrepasar la capacida de la cafetera! Te sobro ".concat(cantidad - this._cantidadActual, "cc de cafe"));
+            this._cantidadActual = this._capacidadMaxima;
+        }
     };
     return Cafetera;
 }());
-var cafetera1 = new Cafetera;
-var cafetera2 = new Cafetera(1250);
-var cafetera3 = new Cafetera(1000, 200);
-var cafetera4 = new Cafetera(1000, 2000);
+// creo las cafeteras comprobando que los requerimientos del constructor se cumplan
+var cafetera1 = new Cafetera; //constructor predeterminado (1000,0)
+var cafetera2 = new Cafetera(1250); //constructor solo capacidad maxima y la actual se iguala a esa capacidad proporcionada (1250,1250)
+var cafetera3 = new Cafetera(1000, 200); // se dan los 2 parametros
+var cafetera4 = new Cafetera(1000, 2000); // se dan los 2 parametros pero la cantidad actual excede la capacidad maxima, se iguala la cantidad a la capacidad (1000,1000)
 console.log(cafetera1);
 console.log(cafetera2);
 console.log(cafetera3);
+console.log(cafetera4);
+// lleno la cafetera
+cafetera1.llenarCafetera();
+// sirvo una taza y trato de servir una taza cuando no tengo tanto cafe
+cafetera3.servirTaza(100);
+cafetera3.servirTaza(200); // sirve la cantidad que puede de cafe
+// vacio la cafetera
+cafetera4.vaciarCafetera();
+console.log(cafetera4);
+console.log("");
+// agrego cafe
+cafetera4.agregarCafe(200);
+console.log(cafetera4);
+cafetera4.agregarCafe(2000);
 console.log(cafetera4);
--- cafetera.ts
+++ cafetera.ts
@@ -38,19 +38,45 @@ class Cafetera {
   }
 
   agregarCafe(cantidad: number) {
-    this._cantidadActual = cantidad;
-    console.log(`Se agrego ${cantidad}cc de cafe.`)
+    if (cantidad <= this._capacidadMaxima) {
+      this._cantidadActual = cantidad;
+      console.log(`Se agrego ${cantidad}cc de cafe.`)
+    } else {
+      console.log(`Trataste de sobrepasar la capacidad de la cafetera! Te sobro ${cantidad - this._cantidadActual}cc de cafe`)
+      this._cantidadActual = this._capacidadMaxima;      
+    }
+    
   }
 
 }
 
-const cafetera1 = new Cafetera;
-const cafetera2 = new Cafetera(1250);
-const cafetera3 = new Cafetera(1000, 200);
-const cafetera4 = new Cafetera(1000, 2000);
+// creo las cafeteras comprobando que los requerimientos del constructor se cumplan
+const cafetera1 = new Cafetera; //constructor predeterminado (1000,0)
+const cafetera2 = new Cafetera(1250); //constructor solo capacidad maxima y la actual se iguala a esa capacidad proporcionada (1250,1250)
+const cafetera3 = new Cafetera(1000, 200); // se dan los 2 parametros
+const cafetera4 = new Cafetera(1000, 2000); // se dan los 2 parametros pero la cantidad actual excede la capacidad maxima, se iguala la cantidad a la capacidad (1000,1000)
 
 console.log(cafetera1);
 console.log(cafetera2);
 console.log(cafetera3);
+console.log(cafetera4);
+
+// lleno la cafetera
+cafetera1.llenarCafetera();
+
+// sirvo una taza y trato de servir una taza cuando no tengo tanto cafe
+cafetera3.servirTaza(100);
+cafetera3.servirTaza(200); // sirve la cantidad que puede de cafe
+
+// vacio la cafetera
+cafetera4.vaciarCafetera();
+console.log(cafetera4);
+console.log("");
+
+// agrego cafe
+cafetera4.agregarCafe(200);
+console.log(cafetera4);
+
+cafetera4.agregarCafe(2000);
 console.log(cafetera4);
 
