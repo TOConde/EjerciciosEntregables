5cefcca2aff4a93767237ed88600813e072e3a3a
TOConde <tomy_t.c@hotmail.com>
2023-09-14 10:35:54 -0300

    atributos y constructor

--- cafetera.ts
+++ cafetera.ts
@@ -1,7 +1,12 @@
 // ejercicio pagina 6, desarrolla una clase Cafetera
 
 class Cafetera {
-  constructor(parameters) {
-    
+  _capacidadMaxima: number;
+  _cantidadActual: number;
+
+  constructor(capacidadMaxima: number = 1000, cantidadActual: number = 0) {
+    this._capacidadMaxima = capacidadMaxima;
+    this._cantidadActual = cantidadActual;
   }
+  
 }
