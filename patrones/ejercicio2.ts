7e775c3c873fc4dbe720f4ed3d6e9dbd04231251
TOConde <tomy_t.c@hotmail.com>
2023-09-15 16:23:47 -0300

    creo las clases principales

--- patrones/ejercicio2.ts
+++ patrones/ejercicio2.ts
@@ -1,3 +1,24 @@
 // Ejercicio 2 de patrones
 // fabrica de vehiculos(autos, camionetas..)
-// vehiculos tienen: marca, modelo y detalles (cantidad de asientos, capacidad de carga, ruedas...)
+// vehiculos tienen: marca, modelo y detalles, como cantidad de asientos, capacidad de carga, ruedas...
+
+
+class FabricarVehiculo {
+  constructor() {
+
+  }
+}
+
+class Vehiculo {
+  constructor(marca: string, modelo: string, color: string, cantidadAsientos: number) {
+    
+  }
+}
+
+interface VehiculoBuilder {
+  setMarca(marca: string): void;
+  setModelo(modelo: string): void;
+  setColor(color: string): void;
+  setAsientos(cantidadAsientos: number): void;
+  build(): Vehiculo;
+}
