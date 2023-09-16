748b18f9b9f77bc220febe45f5b3e13c717e0590
TOConde <tomy_t.c@hotmail.com>
2023-09-16 12:48:10 -0300

    ejercicio 3 de patrones

---
+++ patrones/ejercicio3.js
@@ -0,0 +1,63 @@
+// Ejercicio 3 de patrones
+// log de transacciones de una cuenta
+var TransaccionLog = /** @class */ (function () {
+    function TransaccionLog() {
+        this.transacciones = [];
+    }
+    TransaccionLog.getInstance = function () {
+        if (!TransaccionLog.me) {
+            TransaccionLog.me = new TransaccionLog();
+        }
+        return TransaccionLog.me;
+    };
+    TransaccionLog.prototype.registrarTransaccion = function (monto, moneda, saldo) {
+        var mensaje = "Transaccion reegistrada: Monto: ".concat(monto, " ").concat(moneda, ", Saldo: ").concat(saldo, " U$D");
+        this.transacciones.push(mensaje);
+    };
+    TransaccionLog.prototype.mostrarTransacciones = function () {
+        console.log('Transacciones registradas:');
+        this.transacciones.forEach(function (transaccion, index) {
+            console.log("".concat(index + 1, ". ").concat(transaccion));
+        });
+    };
+    TransaccionLog.me = null;
+    return TransaccionLog;
+}());
+var Cuenta = /** @class */ (function () {
+    function Cuenta(numeroCuenta, saldo) {
+        this.numeroCuenta = numeroCuenta;
+        this.saldo = saldo;
+        this.transaccionLog = TransaccionLog.getInstance();
+    }
+    Cuenta.prototype.conversorMoneda = function (monto, moneda) {
+        var USD = 1;
+        var EUR = 1.07;
+        var ARS = 0.0029; //el oficial
+        var montoUSD;
+        if (moneda === 'EUR') {
+            montoUSD = monto * EUR;
+        }
+        else if (moneda === 'ARS') {
+            montoUSD = monto * ARS;
+        }
+        else {
+            montoUSD = monto * USD;
+        }
+        return montoUSD;
+    };
+    Cuenta.prototype.realizarTransaccion = function (monto, moneda) {
+        var montoUSD = this.conversorMoneda(monto, moneda);
+        this.saldo += montoUSD;
+        this.transaccionLog.registrarTransaccion(monto, moneda, this.saldo);
+    };
+    Cuenta.prototype.consultarSaldo = function () {
+        return this.saldo;
+    };
+    return Cuenta;
+}());
+var log = TransaccionLog.getInstance();
+var cuenta = new Cuenta(123, 1000);
+cuenta.realizarTransaccion(200, "EUR");
+cuenta.realizarTransaccion(-100, "USD");
+cuenta.realizarTransaccion(10000, "ARS");
+log.mostrarTransacciones();
--- patrones/ejercicio3.ts
+++ patrones/ejercicio3.ts
@@ -1,3 +1,76 @@
 // Ejercicio 3 de patrones
-// log de transacciones 
+// log de transacciones de una cuenta
 
+class TransaccionLog {
+  private static me: TransaccionLog | null = null;
+  private transacciones: string[] = [];
+
+  public static getInstance(): TransaccionLog {
+    if (!TransaccionLog.me) {
+      TransaccionLog.me = new TransaccionLog();
+    }    
+    return TransaccionLog.me;
+  }
+
+  public registrarTransaccion(monto: number, moneda: string, saldo: number): void {
+    const mensaje = `Transaccion reegistrada: Monto: ${monto} ${moneda}, Saldo: ${saldo} U$D`
+    this.transacciones.push(mensaje);
+  }
+
+  public mostrarTransacciones(): void {
+    console.log ('Transacciones registradas:')
+    this.transacciones.forEach((transaccion, index) => {
+      console.log(`${index + 1}. ${transaccion}`)
+    })
+  }
+
+}
+
+class Cuenta {
+  private numeroCuenta: number;
+  private saldo: number;
+  private transaccionLog: TransaccionLog;
+
+  constructor(numeroCuenta: number, saldo: number) {
+    this.numeroCuenta = numeroCuenta;
+    this.saldo = saldo;
+    this.transaccionLog = TransaccionLog.getInstance();
+  }
+
+  private conversorMoneda(monto: number, moneda: string): number {
+    const USD: number = 1;
+    const EUR: number = 1.07;
+    const ARS: number = 0.0029 //el oficial
+    let montoUSD: number;
+
+    if (moneda === 'EUR') {
+      montoUSD = monto * EUR;
+    } else if (moneda === 'ARS') {
+      montoUSD = monto * ARS;
+    } else {
+      montoUSD = monto * USD;
+    }
+
+    return montoUSD
+  }
+
+  public realizarTransaccion(monto: number, moneda: string): void {
+    const montoUSD = this.conversorMoneda(monto, moneda)
+    this.saldo += montoUSD;
+    this.transaccionLog.registrarTransaccion(monto, moneda, this.saldo);
+  }
+
+  public consultarSaldo(): number {
+    return this.saldo;
+  }
+
+}
+
+const log = TransaccionLog.getInstance();
+const cuenta = new Cuenta(123, 1000);
+
+cuenta.realizarTransaccion(200, "EUR");
+cuenta.realizarTransaccion(-100, "USD");
+cuenta.realizarTransaccion(10000, "ARS");
+
+log.mostrarTransacciones();
